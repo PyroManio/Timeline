65c6044f3daa68618ed76b3dba8c7ce72a79cf86
PyroManio <90932407+PyroManio@users.noreply.github.com>
2021-11-08 14:48:27 -0800

    Player Animations
    Side idle animations added, and up walking animation added as well.
    Animator properly shows idle animations for all 4 directions.
    Clean up on unused assets.

--- Timeline_v2/Assets/Scripts/PlayerMovement.cs
+++ Timeline_v2/Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private DialogueUI dialogueUI;
 
     public float moveSpeed = 5f;
-
+    
+    // this variable might end up never being used, but it might be helpful
+    public string direction = "Down";
     //NEW
     public DialogueUI DialogueUI => dialogueUI;
     //NEW
@@ -35,10 +37,30 @@ public class PlayerMovement : MonoBehaviour
         // Input
         movement.x = (int)(Input.GetAxisRaw("Horizontal")/0.0625)*0.0625f;
         movement.y = (int)(Input.GetAxisRaw("Vertical")/0.0625)*0.0625f;
-
+        // if player is moving
+        if (!(movement.x == 0 && movement.y == 0))
+        {
+            //animator will remeber that last direction the player was moving before stopping
+            animator.SetFloat("LastHorizontal", movement.x);
+            animator.SetFloat("LastVertical", movement.y);
+            //if player has more horizontal movement
+            if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y) )
+            {
+                if (movement.x > 0) direction = "Right";
+                else direction = "Left";
+            }
+            // if player has more vertical movement
+            else
+            {
+                if (movement.y > 0) direction = "Up";
+                else direction = "Down";
+            }
+        }
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
+
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
     }
 
     void FixedUpdate() 
