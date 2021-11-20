70c05646c3d57d770ed7e7abe51a879e5b39dfe2
PyroManio <90932407+PyroManio@users.noreply.github.com>
2021-11-19 16:47:17 -0800

    Minor Stuff Adding
    ngl I kinda forgot what I added

--- Timeline_v2/Assets/Scripts/ScreenHandler.cs
+++ Timeline_v2/Assets/Scripts/ScreenHandler.cs
@@ -9,21 +9,6 @@ public class ScreenHandler : MonoBehaviour
     {
         changeScreen(0);
     }
-    void LateUpdate()
-    {
-        if(Input.GetKeyDown(KeyCode.O))
-        {
-            screens[0].SetActive(false);
-            screens[1].SetActive(true);
-            //screens[1].TryGetComponent
-            //screens[1].GetComponentInChildren<PuzzleScreen>().setCamera();
-        }
-        if(Input.GetKeyDown(KeyCode.P))
-        {
-            screens[0].SetActive(true);
-            screens[1].SetActive(false);
-        }
-    }
     public void changeScreen(int index){
         for (int i=0; i<screens.Length; i++) screens[i].SetActive(false);
         screens[index].SetActive(true);
