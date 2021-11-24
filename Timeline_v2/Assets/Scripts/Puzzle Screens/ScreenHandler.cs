703c5ae7509b1b5e8cc116e90440d51c790d3fa8
PyroManio <90932407+PyroManio@users.noreply.github.com>
2021-11-24 14:35:00 -0800

    Added Transitions between Screens

--- Timeline_v2/Assets/Scripts/Puzzle Screens/ScreenHandler.cs
+++ Timeline_v2/Assets/Scripts/Puzzle Screens/ScreenHandler.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class ScreenHandler : MonoBehaviour
-{
+{   
+
     [SerializeField] GameObject[] screens;
+    [SerializeField] private TransitionScreen transitionScreen;
     void Start()
     {
         //changeScreen(0);
     }
     public void changeScreen(int index){
         for (int i=0; i<screens.Length; i++) screens[i].SetActive(false);
-        screens[index].SetActive(true);
+            screens[index].SetActive(true);
+        //this is genuinely the worst fix to this stupid damn problem but honestly i am in rage rn
+        transitionScreen.StopAllCoroutines();
+        transitionScreen.halfTransitionItself();
     }
+    public void transChangeScreen(int index)
+    {
+        transitionScreen.fullTransitionItself((index));
+    }
+
 }
