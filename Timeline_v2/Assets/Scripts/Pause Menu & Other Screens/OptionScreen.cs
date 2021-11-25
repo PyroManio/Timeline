fabe80dfc29cd956a7e09220bbe74131a4803a28
PyroManio <90932407+PyroManio@users.noreply.github.com>
2021-11-25 14:36:31 -0800

    Menu Editing Stuff

--- Timeline_v2/Assets/Scripts/Pause Menu & Other Screens/OptionScreen.cs
+++ Timeline_v2/Assets/Scripts/Pause Menu & Other Screens/OptionScreen.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class OptionScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Toggle fullscreenTog;
+    public List<ResItem> resolutions = new List<ResItem>();
+    private int selectedResolution;
+    public TMP_Text resoltuionLabel;
     [SerializeField] private GameObject screenObject;
     public bool IsOpen { get; private set; }
     public void closeScreen(){
@@ -17,6 +21,38 @@ public class OptionScreen : MonoBehaviour
     }
     void Start()
     {
+        //Screen.SetResolution(resolutions[selectedResolution].horizontal,false);
+        fullscreenTog.isOn = Screen.fullScreen;
         closeScreen();
+    }
+    public void ResLeft()
+    {
+        selectedResolution--;
+        if (selectedResolution < 0) selectedResolution = resolutions.Count-1;
+        UpdateResLabel();
+    }
+    public void ResRight()
+    {
+        selectedResolution++;
+        if (selectedResolution > resolutions.Count-1) selectedResolution=0;
+        UpdateResLabel();
+    }
+    public void UpdateResLabel()
+    {
+        resoltuionLabel.text = resolutions[selectedResolution].ToString();
+    }
+    public void ApplyGraphics()
+    {
+        if (fullscreenTog.isOn) {selectedResolution = resolutions.Count-1; UpdateResLabel();}
+        Screen.SetResolution(resolutions[selectedResolution].horizontal,resolutions[selectedResolution].vertical,fullscreenTog.isOn);
+    }
+}
+[System.Serializable]
+public class ResItem
+{
+    public int horizontal, vertical;
+    public override string ToString()
+    {
+        return horizontal.ToString() + " x " + vertical.ToString();
     }
 }
