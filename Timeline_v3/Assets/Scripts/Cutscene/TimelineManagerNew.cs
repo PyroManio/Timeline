7f83c9da6691da57bb86c03bb75dd87e2efc7629
drbloo <jack1234jack5@gmail.com>
2022-04-18 12:12:58 -0700

    added bathroom and some basic colliders, added script for new cutscene system

---
+++ Timeline_v3/Assets/Scripts/Cutscene/TimelineManagerNew.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+using UnityEngine.Events;
+
+public class TimelineManagerNew : MonoBehaviour
+{
+    private PlayableDirector pd;
+    private double duration, currentState;
+
+    [SerializeField] private UnityEvent[] callbacks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pd = GetComponent<PlayableDirector>();
+    }
+
+    public void SetPlayableAsset(PlayableAsset playableAsset)
+    {
+        pd.playableAsset = playableAsset;
+        ResetInitialState();
+    }
+
+    public void PlayCutscene()
+    {
+        pd.Play();
+    }
+
+    public void StopCutscene()
+    {
+        pd.Stop();
+    }
+
+    public void PauseCutscene(double duration)
+    {
+        this.duration = duration;
+        currentState = pd.time;
+        pd.Stop();
+    }
+
+    public void ResumeCutscene()
+    {
+        pd.initialTime = currentState + duration;
+        pd.Play();
+    }
+
+    public void InvokeCallback(int index)
+    {
+        try
+        {
+            callbacks[index]?.Invoke();
+        }
+        catch(System.Exception e)
+        {
+            Debug.LogError("Callback function does not exist. Check the callback index");
+        }
+    }
+
+    public void ResetInitialState()
+    {
+        pd.initialTime = 0;
+    }
+
+}
